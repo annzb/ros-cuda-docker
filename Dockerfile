d1b59fc3b04b790ab5346fd3030c6eeb0e1a358f
annzb <annazabn@gmail.com>
2025-02-11 19:35:25 -0700

    Init building scripts

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+ARG BASE_IMAGE
+FROM ${BASE_IMAGE}
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV LANG=C.UTF-8
+
+
+# OS
+RUN set -eux; \
+    UBUNTU_CODENAME=$(grep -oP '(?<=UBUNTU_CODENAME=)\w+' /etc/os-release); \
+    echo "deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME} main restricted universe multiverse" > /etc/apt/sources.list; \
+    echo "deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME}-updates main restricted universe multiverse" >> /etc/apt/sources.list; \
+    echo "deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME}-backports main restricted universe multiverse" >> /etc/apt/sources.list; \
+    echo "deb http://security.ubuntu.com/ubuntu ${UBUNTU_CODENAME}-security main restricted universe multiverse" >> /etc/apt/sources.list; \
+    apt update && apt upgrade -y && \
+    apt install --no-install-recommends -y \
+        apt-utils \
+        wget \
+        lsb-release \
+        gnupg \
+        software-properties-common \
+        build-essential \
+        cmake
+
+
+# CUDA
+ARG CUDA_VERSION=""
+RUN if [ -n "$CUDA_VERSION" ]; then \
+    set -eux; \
+    CUDA_VERSION_X_Y=$(echo "$CUDA_VERSION" | awk -F. '{print $1"-"$2}'); \
+    apt update && apt install --no-install-recommends -y cuda-toolkit-${CUDA_VERSION_X_Y} && \
+    nvcc --version; \
+fi
+
+
+# ROS
+ARG ROS_DISTRO=""
+RUN if [ -n "$ROS_DISTRO" ]; then \
+    wget -qO - https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | apt-key add - && \
+    if [ "$ROS_DISTRO" = "noetic" ]; then \
+        echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list; \
+    else \
+        echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros2-latest.list; \
+    fi && \
+    apt update && apt install --no-install-recommends -y \
+        ros-${ROS_DISTRO}-desktop-full \
+        python3-rosbag \
+        python3-rosdep \
+        python3-colcon-common-extensions && \
+    rosdep init && rosdep update && \
+    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> /etc/bash.bashrc; \
+fi
+
+
+CMD ["bash"]
---
+++ build_image.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+# Check dependencies
+for cmd in yq nvcc python3 docker; do
+    if ! command -v $cmd &> /dev/null; then
+        echo "Error: $cmd is not installed."
+        case $cmd in
+            yq) echo "Please run 'apt install yq'";;
+            nvcc) echo "Please install the NVIDIA CUDA Toolkit.";;
+            python3) echo "Please install Python 3.";;
+            docker) echo "Please install Docker.";;
+        esac
+        exit 1
+    fi
+done
+
+
+# Validate configuration
+CONFIG_FILE="ros-versions.yaml"
+PYTHON_SCRIPT="utils/find_base_image.py"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file '$CONFIG_FILE' not found."
+    exit 1
+fi
+if [ ! -f "$PYTHON_SCRIPT" ]; then
+    echo "Error: Python script '$PYTHON_SCRIPT' not found."
+    exit 1
+fi
+
+
+# Set ROS distribution
+AVAILABLE_ROS_DISTROS=$(yq '.ros_versions | keys | map(select(. != "default")) | join(" ")' "$CONFIG_FILE")
+ROS_DISTRO="${1:-}"
+if [ -n "$ROS_DISTRO" ] && ! yq -e ".ros_versions | has(\"$ROS_DISTRO\")" "$CONFIG_FILE" > /dev/null; then
+    echo "Error: Invalid ROS distribution '$ROS_DISTRO'."
+    echo "Available options: $AVAILABLE_ROS_DISTROS"
+    exit 1
+fi
+echo "Using ROS distribution: '${ROS_DISTRO:-default}'"
+
+
+# Set CUDA version
+DOCKER_CUDA_VERSION=$(nvcc --version | grep -oP "release \K[0-9]+\.[0-9]+" || true)
+DOCKER_CUDA_VERSION=${DOCKER_CUDA_VERSION:-""}
+echo "Using CUDA version: ${DOCKER_CUDA_VERSION:-None}"
+
+
+# Set base image
+BASE_IMAGE=$(python3 "$PYTHON_SCRIPT" --cuda "$DOCKER_CUDA_VERSION" --ros "$ROS_DISTRO")
+if [ -z "$BASE_IMAGE" ]; then
+    echo "Error: Failed to determine base image."
+    exit 1
+fi
+echo "Using base image: $BASE_IMAGE"
+
+
+# Construct image name
+UBUNTU_VERSION=$(echo "$BASE_IMAGE" | grep -oP "ubuntu:?\K[0-9]+\.[0-9]+" || true)
+UBUNTU_VERSION=${UBUNTU_VERSION:-""}
+OS_IMAGE_NAME="ubuntu:${UBUNTU_VERSION:-latest}"
+[ -n "$DOCKER_CUDA_VERSION" ] && OS_IMAGE_NAME+="-cuda$DOCKER_CUDA_VERSION"
+[ -n "$ROS_DISTRO" ] && OS_IMAGE_NAME+="-$ROS_DISTRO"
+echo "Building OS image: $OS_IMAGE_NAME"
+
+
+# Build image
+docker build \
+    --build-arg BASE_IMAGE="$BASE_IMAGE" \
+    --build-arg ROS_DISTRO="$ROS_DISTRO" \
+    -t "$OS_IMAGE_NAME" .
+echo "$OS_IMAGE_NAME"
